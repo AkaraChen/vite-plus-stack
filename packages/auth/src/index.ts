20995ab56d9bc5aa87ccdc196439aff7aadaa043
akarachen <akarachen@outlook.com>
2026-03-14 20:14:31 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import { db } from "@vite-plus-stack/db";
+import * as schema from "@vite-plus-stack/db/schema/auth";
+import { env } from "@vite-plus-stack/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [tanstackStartCookies()],
+});
